b90e8ed045b2b0391bcf396eb3b938d339434f1e
Wadood Hussaini <53196769+Wadoodh@users.noreply.github.com>
2022-02-16 08:44:06 -0500

    show user info

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function getUser() {
-  wf_utils.isLoggedIn((user) => {
-    console.log(user);
+  wf_utils.isLoggedIn((loggedIn) => {
+    if (loggedIn) {
+      wf_utils.getUser((user) => {
+        let email = user.email;
+        let name = user.name;
+
+        console.log(user);
+      });
+    }
   });
 }
